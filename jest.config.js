ca16555225f0ac0dfe046b348157d5ca570f3648
Kittiphong <kittiphongss63@gmail.com>
2026-04-21 10:56:05 +0700

    update test for 100 coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   testMatch: ['**/tests/**/*.test.js'],
+  collectCoverageFrom: [
+    'controllers/restaurants.js',
+    'controllers/reservations.js',
+    'models/Restaurant.js',
+    'models/Reservation.js',
+  ],
 };
